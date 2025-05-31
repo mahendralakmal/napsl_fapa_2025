ea76741eab65f862d0e31775d2975d5c1cdb1647
mahendra <mahendralakmal@gmail.com>
2025-05-31 08:32:09 +0530

    Add image carousel to index view and update related styles

--- storage/framework/views/14dba99526753950c964f17eb0fe5947.php
+++ storage/framework/views/14dba99526753950c964f17eb0fe5947.php
@@ -20,12 +20,62 @@
         .btn:hover {
             background-color: #0056b3;
         }
+
+        /* .carousel-item {
+            width: 50% !important;
+            height: auto;
+        } */
     </style>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('content'); ?>
-
-
-
+<div class="d-flex justify-content-center">
+    <div id="carouselExampleIndicators" class="carousel slide mb-4" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7"></button>
+            
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 1.jpg')); ?>" class="d-block w-100" alt="Image 1">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 2.jpg')); ?>" class="d-block w-100" alt="Image 2">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 3.jpg')); ?>" class="d-block w-100" alt="Image 3">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 4.jpg')); ?>" class="d-block w-100" alt="Image 3">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 5.jpg')); ?>" class="d-block w-100" alt="Image 3">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 6.jpg')); ?>" class="d-block w-100" alt="Image 3">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 7.jpg')); ?>" class="d-block w-100" alt="Image 3">
+            </div>
+            
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('images/carusel/FAPA Web 9.jpg')); ?>" class="d-block w-100" alt="Image 3">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+    </div>
+</div>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('script'); ?>
     <!-- apexcharts -->
