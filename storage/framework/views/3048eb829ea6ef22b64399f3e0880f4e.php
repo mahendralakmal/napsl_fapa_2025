396ac37e45884985d245d92c9354f7939f94c4b8
mahendra <mahendralakmal@gmail.com>
2025-06-01 23:03:49 +0530

    Stop tracking storage/framework/views and cache

--- storage/framework/views/3048eb829ea6ef22b64399f3e0880f4e.php
+++
@@ -1,8 +0,0 @@
-
-
-
-<body style="background-color: #efefef !important;">
-
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH /var/www/napsl_ex/resources/views/layouts/body.blade.php ENDPATH**/ ?>
--- storage/framework/views/cb84438fa2d622cdfd39b42776b3bfb6.php
+++
@@ -1,5 +0,0 @@
-
-<body style="background-color: transparent !important;">
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH /var/www/napsl_exhibitions/resources/views/layouts/body.blade.php ENDPATH**/ ?>
